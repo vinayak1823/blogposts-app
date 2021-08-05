576b07585ad6f84d537cd0202f1c8bc73c2a2b12
vinayak1823 <vinayak18dec@gmail.com>
2021-08-05 16:39:12 +0530

    Ready for deployment

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blog Post App
+
+It is a basic MERN app which allows users to add posts as well as edit them.
+
+## Features
+
+- Add posts
+- Remove posts
+- Update posts
--- server.js
+++ server.js
@@ -14,6 +14,17 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/post', postroute)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
+
 app.listen(process.env.PORT, function () {
   console.log('Node started')
 })
